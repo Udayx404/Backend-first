e2e395b99c95591cff901e00c48690b851bda4d3
Udayx404 <udaygit88@gmail.com>
2025-10-18 13:15:03 +0530

    edit 5

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"; 
 
+
 dotenv.config({
     path: "./env"
 })
 connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+        console.log(error)
+        throw error
+        })
 
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`App running at port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log(error)
+})
 
 
 // const app = express()
